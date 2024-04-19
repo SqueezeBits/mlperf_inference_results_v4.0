0bb171ae62c80e2d9e7c001897f9cc34873d4edb
Minkyu Kim <minkyu.kim@squeezebits.com>
2024-04-19 01:47:27 +0000

    chore: add benchmark scripts with DIBT dataset

---
+++ closed/Intel-HabanaLabs/code/llama2-70b-99.9/llama/run_generation.py
@@ -0,0 +1,81 @@
+import argparse
+import requests
+import time
+from typing import List
+import json
+
+from datasets import load_dataset
+from transformers import AutoTokenizer
+
+from tgi_client import TgiClient
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--server_address", type=str, default="http://localhost:8484", help="Address of the TGI server"
+    )
+    parser.add_argument(
+        "--model_id", type=str, default="/mnt/weka/data/pytorch/llama2/Llama-2-7b-chat-hf", help="Model id used in TGI server"
+    )
+    parser.add_argument(
+        "--max_input_length", type=int, default=1024, help="Max input length for TGI model"
+    )
+    parser.add_argument(
+        "--max_output_length", type=int, default=1024, help="Max output length for TGI model"
+    )
+    parser.add_argument(
+        "--total_sample_count", type=int, default=2, help="Total number of samples to generate"
+    )
+    parser.add_argument(
+        "--max_concurrent_requests", type=int, default=128, help="Max number of concurrent requests"
+    )
+    return parser.parse_args()
+
+
+def read_dataset(
+    max_input_length: int,
+    total_sample_count: int,
+    model_id: str
+) -> List[str]:
+    with open('DIBT_dataset_len256.json', 'r', encoding='utf-8') as f:
+            input_sentences = json.load(f)
+    return input_sentences[:total_sample_count]
+
+
+def is_tgi_available(
+    server_address: str
+) -> bool:
+    """
+    Checks if TGI server is available under the specified address.
+    """
+    try:
+        info = requests.get(f"{server_address}/info")
+        return info.status_code == 200
+    except:
+        return False
+
+
+def main():
+    args = get_args()
+    dataset = read_dataset(
+        args.max_input_length, args.total_sample_count, args.model_id
+    )
+
+    if not is_tgi_available(args.server_address):
+        raise RuntimeError("Cannot connect with TGI server!")
+
+    tgi_client = TgiClient(
+        args.server_address, args.max_concurrent_requests
+    )
+    timestamp = time.perf_counter_ns()
+    tgi_client.run_generation(
+        dataset, args.max_output_length
+    )
+    duration_s = (time.perf_counter_ns() - timestamp) * 1e-9
+    tgi_client.print_performance_metrics(duration_s)
+
+
+if __name__ == '__main__':
+    main()
+
